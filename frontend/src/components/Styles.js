4dcf250f00ad9a45a19d0653922cbac8395ea442
Osagie Omigie <osagieomigie@gmail.com>
2020-03-26 17:09:45 -0600

    Added basic user registration form

--- frontend/src/components/Styles.js
+++ frontend/src/components/Styles.js
@@ -1,10 +1,13 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles(theme => ({
-  root: {
+  formRoot: {
     "& .MuiTextField-root": {
       margin: theme.spacing(1),
       width: "25ch"
+    },
+    root: {
+      margin: "5%"
     }
   }
 }));
--- frontend/src/components/loginForm.js
+++ frontend/src/components/loginForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useStyles } from "./Styles";
 import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
 
 function LoginForm() {
   const classes = useStyles();
@@ -8,29 +9,31 @@ function LoginForm() {
   const [password, setPassword] = useState(""); // password hook
 
   return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <div>
-        <TextField
-          required
-          id="standard-required"
-          label="Username"
-          defaultValue="Enter username"
-          value={user}
-          onChange={e => setUser(e.target.value)}
-        />
-      </div>
-      <div>
-        <TextField
-          required
-          id="standard-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-      </div>
-    </form>
+    <Paper className={classes.root}>
+      <form className={classes.formRoot} noValidate autoComplete="off">
+        <div>
+          <TextField
+            required
+            id="standard-required"
+            label="Username"
+            defaultValue="Enter username"
+            value={user}
+            onChange={e => setUser(e.target.value)}
+          />
+        </div>
+        <div>
+          <TextField
+            required
+            id="standard-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+      </form>
+    </Paper>
   );
 }
 
