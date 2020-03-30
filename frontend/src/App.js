9fb4028ee2568ccb7d2b74f981bd32ebc839ba12
Osagie Omigie <osagieomigie@gmail.com>
2020-03-29 22:56:38 -0600

    Added react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,17 @@ import "./App.css";
 import LoginForm from "./components/loginForm.js";
 import RegistrationForm from "./components/registrationForm.js";
 import DealTile from "./components/dealTile.js";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      {/*<LoginForm/>*/}
-      {/*<RegistrationForm /> */}
-      <DealTile />
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/" exact component={LoginForm} />
+        <Route path="/register" component={RegistrationForm} />
+        <Route path="/deal" component={DealTile} />
+      </div>
+    </Router>
   );
 }
 
