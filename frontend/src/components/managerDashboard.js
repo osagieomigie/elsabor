e39e57c1d0ef5857d0a409628933546aa6d2c5d4
Osagie Omigie <osagieomigie@gmail.com>
2020-04-19 14:59:05 -0600

    Store page now displays its current deals

--- frontend/src/components/managerDashboard.js
+++ frontend/src/components/managerDashboard.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Carousel from "react-material-ui-carousel";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import DealTile from "./dealTile.js";
-import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import PersistentDrawerLeft from "./searchHeader.js";
 import Card from "@material-ui/core/Card";
@@ -15,20 +15,42 @@ const useStyles = makeStyles((theme) => ({
     paddingLeft: "10%",
     paddingRight: "10%",
   },
-  textA: {
+  dealStyle: {
+    marginTop: "10%",
     textAlign: "left",
     marginBottom: "0%",
+    [theme.breakpoints.down("sm")]: {
+      marginTop: "35%",
+      marginBottom: "3%",
+    },
+    [theme.breakpoints.between("sm", "md")]: {
+      marginTop: "15%",
+      marginBottom: "0%",
+    },
   },
   textB: {
     textAlign: "left",
     marginBottom: "1%",
+    [theme.breakpoints.down("sm")]: {
+      marginTop: "3%",
+      marginBottom: "5%",
+    },
+    [theme.breakpoints.between("sm", "md")]: {
+      marginBottom: "3%",
+    },
   },
   card: {
     minWidth: 290,
     minHeight: 300,
     maxWidth: 345,
-    marginLeft: "5%",
+    marginLeft: "0.8%",
     marginBottom: "10%",
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: "1.8%",
+    },
+    [theme.breakpoints.between("sm", "md")]: {
+      marginLeft: "1%",
+    },
   },
   linkStyle: {
     color: "inherit",
@@ -36,84 +58,115 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-let savedDeals = [
-  {
-    deal: "Corona Beer",
-    description: "this is just a place holder, chill",
-    qrCode: "QR Code",
-    expiryDate: "20-12-20",
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    slidesToSlide: 3, // optional, default to 1.
   },
-  {
-    deal: "Chicken",
-    description: "this is just a place holder, chill",
-    qrCode: "QR Code",
-    expiryDate: "20-12-20",
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 2, // optional, default to 1.
   },
-  {
-    deal: "GOAT",
-    description: "this is just a place holder, chill",
-    qrCode: "QR Code",
-    expiryDate: "20-12-20",
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1, // optional, default to 1.
   },
-];
+};
 
 function ManagerDashboard() {
   const classes = useStyles();
   const { userId } = queryString.parse(window.location.search); // extract userId
+  const proxyurl = "https://cors-anywhere.herokuapp.com/";
+  const p2 = "https://elsabor-cors.herokuapp.com/";
+
   console.log(`userID ${userId}`);
+  const [storeDeals, setStoreDeals] = useState([]);
+
+  // get store deals
+  const getStoreDeals = () => {
+    console.log(`extracted userID ${userId}`);
+    fetch(p2 + "https://elsabor.herokuapp.com/users/getSavedDeals", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `userid=${userId}`,
+    })
+      .then((response) => {
+        console.log(`Status code ${response.status}`);
+        response.text().then((result) => {
+          console.log(result);
+          setStoreDeals(JSON.parse(result));
+        });
+      })
+      .catch((error) => {
+        console.error("Error: ", error);
+      });
+  };
+
+  useEffect(() => {
+    getStoreDeals();
+    // eslint-disable-next-line
+  }, [userId]);
+
   return (
     <div className={classes.root}>
       <PersistentDrawerLeft />
-      <Typography
-        variant="h5"
-        gutterBottom
-        className={classes.textA}
-        style={{ marginTop: "10%" }}
-      >
+      <Typography variant="h5" gutterBottom className={classes.dealStyle}>
         Promotions
       </Typography>
       <Typography variant="subtitle1" gutterBottom className={classes.textB}>
-        Manage your promotions and add new ones.
+        Manage your promotions and add new ones!
       </Typography>
 
       <div className={classes.favourites}>
-        <Carousel>
-          <Grid item xs={12}>
-            <Grid container justify="center">
-              {savedDeals.map(
-                ({ deal, description, qrCode, expiryDate }, index) => (
-                  <DealTile
-                    deal={deal}
-                    description={description}
-                    qrCode={qrCode}
-                    expiryDate={expiryDate}
-                    pictureLink={
-                      "https://cdn.pixabay.com/photo/2018/10/05/23/24/chicken-3727097__340.jpg"
-                    }
-                    key={index}
-                  />
-                )
-              )}
-            </Grid>
-          </Grid>
+        <Carousel
+          swipeable={true}
+          draggable={true}
+          showDots={true}
+          responsive={responsive}
+          ssr={true} // means to render carousel on server-side.
+          autoPlaySpeed={1000}
+          keyBoardControl={true}
+          customTransition="all .5"
+          transitionDuration={500}
+          containerClass="carousel-container"
+          removeArrowOnDeviceType={["tablet", "mobile"]}
+          dotListClass="custom-dot-list-style"
+          itemClass="carousel-item-padding-40-px"
+        >
+          {storeDeals.map(({ dealid, name, desp, link, expiry }, index) => (
+            <DealTile
+              dealId={dealid}
+              deal={name}
+              description={desp}
+              qrCode={dealid}
+              expiryDate={expiry}
+              pictureLink={link}
+              key={index}
+            />
+          ))}
         </Carousel>
-      </div>
 
-      <Link to={`/addCoupon`} className={classes.linkStyle}>
-        <Card className={classes.card}>
-          <img
-            component="img"
-            height="250"
-            src={require("./Assets/plus-icon.png")}
-            title="QR code"
-          />
-          <CardContent>
-            <Typography variant="subtitle1" gutterBottom>
-              Add new deal
-            </Typography>
-          </CardContent>
-        </Card>
-      </Link>
+        <Link to={`/addCoupon?userId=${userId}`} className={classes.linkStyle}>
+          <Card className={classes.card}>
+            <img
+              component="img"
+              height="250"
+              src={require("./Assets/plus-icon.png")}
+              title="QR code"
+            />
+            <CardContent>
+              <Typography variant="subtitle1" gutterBottom>
+                Add new deal
+              </Typography>
+            </CardContent>
+          </Card>
+        </Link>
+      </div>
     </div>
   );
 }
